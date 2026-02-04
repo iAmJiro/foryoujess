1f8c92b1fdaab287169e38491f0749e91a22a27e
iAmJiro <115590354+iAmJiro@users.noreply.github.com>
2026-02-04 14:15:33 +1300

    builtfordeploy

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home";
+
+createRoot(document.getElementById("root")).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>,
+);
